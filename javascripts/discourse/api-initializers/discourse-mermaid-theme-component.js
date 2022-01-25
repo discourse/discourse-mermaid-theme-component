3671fbe44d40eda4d6df1402d91816a00ce08899
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2022-01-25 16:35:37 +0100

    DEV: uses id

--- javascripts/discourse/api-initializers/discourse-mermaid-theme-component.js
+++ javascripts/discourse/api-initializers/discourse-mermaid-theme-component.js
@@ -3,41 +3,44 @@ import loadScript from "discourse/lib/load-script";
 import { apiInitializer } from "discourse/lib/api";
 
 export default apiInitializer("0.11.1", (api) => {
-  api.decorateCookedElement(async (elem, helper) => {
-    const mermaids = elem.querySelectorAll(
-      "[data-wrap=mermaid]:not([data-processed=true])"
-    );
-
-    if (!mermaids.length) {
-      return;
-    }
-
-    mermaids.forEach((mermaid) => {
-      mermaid.style.height = `${mermaid.dataset.height || 400}px`;
-
-      const spinner = document.createElement("div");
-      spinner.classList.add("spinner");
-      mermaid.append(spinner);
-    });
-
-    await loadScript("https://unpkg.com/mermaid@8.13.10/dist/mermaid.min.js");
-
-    window.mermaid.initialize({
-      startOnLoad: false,
-      theme: Session.current().userDarkSchemeId > 0 ? "dark" : "default",
-    });
-
-    mermaids.forEach((mermaid, index) => {
-      const key = helper ? `post_${helper.getModel().id}` : "composer";
-
-      window.mermaid.mermaidAPI.render(
-        `mermaid_${index}_${key}`,
-        mermaid.innerText,
-        (svgCode) => {
-          mermaid.dataset.processed = true;
-          mermaid.innerHTML = svgCode;
-        }
+  api.decorateCookedElement(
+    async (elem, helper) => {
+      const mermaids = elem.querySelectorAll(
+        "[data-wrap=mermaid]:not([data-processed=true])"
       );
-    });
-  });
+
+      if (!mermaids.length) {
+        return;
+      }
+
+      mermaids.forEach((mermaid) => {
+        mermaid.style.height = `${mermaid.dataset.height || 400}px`;
+
+        const spinner = document.createElement("div");
+        spinner.classList.add("spinner");
+        mermaid.append(spinner);
+      });
+
+      await loadScript("https://unpkg.com/mermaid@8.13.10/dist/mermaid.min.js");
+
+      window.mermaid.initialize({
+        startOnLoad: false,
+        theme: Session.current().userDarkSchemeId > 0 ? "dark" : "default",
+      });
+
+      mermaids.forEach((mermaid, index) => {
+        const key = helper ? `post_${helper.getModel().id}` : "composer";
+
+        window.mermaid.mermaidAPI.render(
+          `mermaid_${index}_${key}`,
+          mermaid.innerText,
+          (svgCode) => {
+            mermaid.dataset.processed = true;
+            mermaid.innerHTML = svgCode;
+          }
+        );
+      });
+    },
+    { id: "discourse-mermaid-theme-component" }
+  );
 });
