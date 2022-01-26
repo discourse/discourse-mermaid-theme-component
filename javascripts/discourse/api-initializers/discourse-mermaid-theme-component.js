9dd68c97da7cb231feb7deecae11edf02c54099f
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2022-01-26 14:09:29 +0100

    FIX: more resilient error handling
    Part of this commit also attempts to help user avoid errors.

--- javascripts/discourse/api-initializers/discourse-mermaid-theme-component.js
+++ javascripts/discourse/api-initializers/discourse-mermaid-theme-component.js
@@ -1,8 +1,38 @@
 import Session from "discourse/models/session";
 import loadScript from "discourse/lib/load-script";
 import { apiInitializer } from "discourse/lib/api";
+import I18n from "I18n";
 
 export default apiInitializer("0.11.1", (api) => {
+  api.addToolbarPopupMenuOptionsCallback(() => {
+    return {
+      action: "insertMermaidSample",
+      icon: "project-diagram",
+      label: themePrefix("insert_mermaid_sample"),
+    };
+  });
+
+  // this is a hack as applySurround expects a top level
+  // composer key, not possible from a theme
+  window.I18n.translations[
+    window.I18n.locale
+  ].js.composer.mermaid_sample = `    flowchart
+         A --> B`;
+
+  api.modifyClass("controller:composer", {
+    pluginId: "discourse-mermaid-theme-component",
+    actions: {
+      insertMermaidSample() {
+        this.toolbarEvent.applySurround(
+          "\n" + `[wrap="mermaid"]` + "\n",
+          "\n[/wrap]\n",
+          "mermaid_sample",
+          { multiline: false }
+        );
+      },
+    },
+  });
+
   api.decorateCookedElement(
     async (elem, helper) => {
       const mermaids = elem.querySelectorAll(
@@ -29,16 +59,31 @@ export default apiInitializer("0.11.1", (api) => {
       });
 
       mermaids.forEach((mermaid, index) => {
-        const key = helper ? `post_${helper.getModel().id}` : "composer";
+        try {
+          const content = mermaid.querySelector("code")?.textContent;
 
-        window.mermaid.mermaidAPI.render(
-          `mermaid_${index}_${key}`,
-          mermaid.innerText,
-          (svgCode) => {
-            mermaid.dataset.processed = true;
-            mermaid.innerHTML = svgCode;
+          if (!content) {
+            throw "invalid content";
           }
-        );
+
+          if (window.mermaid.parse(content)) {
+            let key = helper ? `post_${helper.getModel().id}` : "composer";
+            window.mermaid.mermaidAPI.render(
+              `mermaid_${index}_${key}`,
+              content,
+              (svg) => {
+                mermaid.innerHTML = svg;
+              }
+            );
+          }
+        } catch (e) {
+          mermaid.innerHTML = `<span class="mermaid-error">${I18n.t(
+            themePrefix("rendering_error")
+          )}</span>`;
+        } finally {
+          mermaid.dataset.processed = true;
+          mermaid.querySelector(".spinner")?.remove();
+        }
       });
     },
     { id: "discourse-mermaid-theme-component" }
