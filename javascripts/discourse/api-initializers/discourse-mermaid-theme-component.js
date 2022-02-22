9aeaa9fd022dbf6d27a33936f0eb1dc8cb8125a8
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2022-02-22 16:28:21 +0100

    FEATURE: allows to use mermaid in discourse chat

--- javascripts/discourse/api-initializers/discourse-mermaid-theme-component.js
+++ javascripts/discourse/api-initializers/discourse-mermaid-theme-component.js
@@ -1,6 +1,62 @@
-import Session from "discourse/models/session";
 import loadScript from "discourse/lib/load-script";
 import { apiInitializer } from "discourse/lib/api";
+
+async function applyMermaid(element, id) {
+  const mermaids = element.querySelectorAll("pre[data-code-wrap=mermaid]");
+
+  if (!mermaids.length) {
+    return;
+  }
+
+  await loadScript("https://unpkg.com/mermaid@8.13.10/dist/mermaid.min.js");
+
+  window.mermaid.initialize({
+    startOnLoad: false,
+    theme:
+      getComputedStyle(document.body)
+        .getPropertyValue("--scheme-type")
+        .trim() === "dark"
+        ? "dark"
+        : "default",
+  });
+
+  mermaids.forEach((mermaid) => {
+    const spinner = document.createElement("div");
+    spinner.classList.add("spinner");
+
+    if (mermaid.dataset.codeHeight) {
+      mermaid.style.height = `${mermaid.dataset.codeHeight}px`;
+    }
+
+    mermaid.append(spinner);
+  });
+
+  mermaids.forEach((mermaid, index) => {
+    const code = mermaid.querySelector("code");
+
+    if (!code) {
+      return;
+    }
+
+    try {
+      if (window.mermaid.parse(code.textContent || "")) {
+        let key = id ? id : "composer";
+        window.mermaid.mermaidAPI.render(
+          `mermaid_${index}_${key}`,
+          code.textContent || "",
+          (svg) => {
+            mermaid.innerHTML = svg;
+          }
+        );
+      }
+    } catch (e) {
+      mermaid.innerHTML = `<span class="mermaid-error">${e?.str || e}</span>`;
+    } finally {
+      mermaid.dataset.processed = true;
+      mermaid.querySelector(".spinner")?.remove();
+    }
+  });
+}
 
 export default apiInitializer("0.11.1", (api) => {
   api.addToolbarPopupMenuOptionsCallback(() => {
@@ -32,64 +88,16 @@ export default apiInitializer("0.11.1", (api) => {
     },
   });
 
+  if (api.decorateChatMessage) {
+    api.decorateChatMessage((message) => {
+      applyMermaid(message, `chat_message_${message.id}`);
+    });
+  }
+
   api.decorateCookedElement(
     async (elem, helper) => {
-      const mermaids = elem.querySelectorAll("pre[data-code-wrap=mermaid]");
-
-      if (!mermaids.length) {
-        return;
-      }
-
-      await loadScript("https://unpkg.com/mermaid@8.13.10/dist/mermaid.min.js");
-
-      window.mermaid.initialize({
-        startOnLoad: false,
-        theme:
-          getComputedStyle(document.body)
-            .getPropertyValue("--scheme-type")
-            .trim() === "dark"
-            ? "dark"
-            : "default",
-      });
-
-      mermaids.forEach((mermaid) => {
-        const spinner = document.createElement("div");
-        spinner.classList.add("spinner");
-
-        if (mermaid.dataset.codeHeight) {
-          mermaid.style.height = `${mermaid.dataset.codeHeight}px`;
-        }
-
-        mermaid.append(spinner);
-      });
-
-      mermaids.forEach((mermaid, index) => {
-        const code = mermaid.querySelector("code");
-
-        if (!code) {
-          return;
-        }
-
-        try {
-          if (window.mermaid.parse(code.textContent || "")) {
-            let key = helper ? `post_${helper.getModel().id}` : "composer";
-            window.mermaid.mermaidAPI.render(
-              `mermaid_${index}_${key}`,
-              code.textContent || "",
-              (svg) => {
-                mermaid.innerHTML = svg;
-              }
-            );
-          }
-        } catch (e) {
-          mermaid.innerHTML = `<span class="mermaid-error">${
-            e?.str || e
-          }</span>`;
-        } finally {
-          mermaid.dataset.processed = true;
-          mermaid.querySelector(".spinner")?.remove();
-        }
-      });
+      const id = helper ? `post_${helper.getModel().id}` : "composer";
+      applyMermaid(elem, id);
     },
     { id: "discourse-mermaid-theme-component" }
   );
