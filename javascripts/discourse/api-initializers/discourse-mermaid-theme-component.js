171171e2c8ea10781b876d86a501a1d548cdc579
Sam <sam.saffron@gmail.com>
2023-08-01 11:17:09 +1000

    FIX: remove injected error element (#18)
    Mermaid has an error renderer we do not use, make sure we clean up.
    
    Previous to this change errors would inject an element on the page which would
    show up at the bottom below the footer.
    
    * parse is async, so there is an unhandled promise, no point parsing, just
    pass it through...

--- javascripts/discourse/api-initializers/discourse-mermaid-theme-component.js
+++ javascripts/discourse/api-initializers/discourse-mermaid-theme-component.js
@@ -43,23 +43,21 @@ async function applyMermaid(element, key = "composer") {
       return;
     }
 
-    if (window.mermaid.parse(code.textContent || "")) {
-      const promise = window.mermaid.render(
-        `mermaid_${index}_${key}`,
-        code.textContent || ""
-      );
-      promise
-        .then((object) => {
-          mermaid.innerHTML = object.svg;
-        })
-        .catch((e) => {
-          mermaid.innerText = e?.message || e;
-        })
-        .finally(() => {
-          mermaid.dataset.processed = true;
-          mermaid.querySelector(".spinner")?.remove();
-        });
-    }
+    const mermaidId = `mermaid_${index}_${key}`;
+    const promise = window.mermaid.render(mermaidId, code.textContent || "");
+    promise
+      .then((object) => {
+        mermaid.innerHTML = object.svg;
+      })
+      .catch((e) => {
+        mermaid.innerText = e?.message || e;
+        // mermaid injects an error element, we need to remove it
+        document.getElementById(mermaidId)?.remove();
+      })
+      .finally(() => {
+        mermaid.dataset.processed = true;
+        mermaid.querySelector(".spinner")?.remove();
+      });
 
     if (key === "composer") {
       discourseDebounce(updateMarkdownHeight, mermaid, index, 500);
