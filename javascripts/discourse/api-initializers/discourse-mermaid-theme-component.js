22dc7c14c24aab23a8baae84905e730da3a10420
David Taylor <david@taylorhq.com>
2025-02-10 17:13:12 +0000

    UX: Make diagrams fixed-height, with fullscreen view
    The variable-height strategy used by this theme component is complex, and is not providing a good user experience. This commit removes it, and instead sets all mermaid diagrams to have a fixed aspect-ratio of `16:9` when rendered in posts. For tall diagrams, the content will be scrollable. A fullscreen button in the top right allows popping the diagram into a modal.

--- javascripts/discourse/api-initializers/discourse-mermaid-theme-component.js
+++ javascripts/discourse/api-initializers/discourse-mermaid-theme-component.js
@@ -1,94 +1,40 @@
 import { apiInitializer } from "discourse/lib/api";
-import discourseDebounce from "discourse/lib/debounce";
-import loadScript from "discourse/lib/load-script";
+import { generateDiagram } from "../components/mermaid-diagram";
+import MermaidInline from "../components/mermaid-inline";
 
-async function applyMermaid(element, key = "composer", container) {
-  const mermaids = element.querySelectorAll("pre[data-code-wrap=mermaid]");
-
-  if (!mermaids.length) {
+function applyMermaid(mermaidPre, helper) {
+  const mermaidSrc = mermaidPre.querySelector("code")?.textContent;
+  if (!mermaidSrc) {
     return;
   }
 
-  await loadScript(settings.theme_uploads_local.mermaid_js);
+  const mermaidWrapper = document.createElement("div");
+  mermaidWrapper.classList.add("mermaid-wrapper");
 
-  window.mermaid.initialize({
-    startOnLoad: false,
-    theme:
-      getComputedStyle(document.body)
-        .getPropertyValue("--scheme-type")
-        .trim() === "dark"
-        ? "dark"
-        : "default",
-  });
+  if (helper?.renderGlimmer) {
+    helper.renderGlimmer(mermaidWrapper, MermaidInline, {
+      src: mermaidSrc,
+    });
+  } else {
+    // Legacy support for parts of core which cannot renderGlimmer. No fullscreen support
+    const mermaidDiagram = document.createElement("div");
+    mermaidDiagram.classList.add("mermaid-diagram");
+    mermaidDiagram.innerHTML = "<div class='spinner'></div>";
+    mermaidWrapper.appendChild(mermaidDiagram);
 
-  mermaids.forEach((mermaid) => {
-    if (mermaid.dataset.processed) {
-      return;
-    }
-
-    const spinner = document.createElement("div");
-    spinner.classList.add("spinner");
-
-    if (mermaid.dataset.codeHeight && key !== "composer") {
-      mermaid.style.height = `${mermaid.dataset.codeHeight}px`;
-    }
-
-    mermaid.append(spinner);
-  });
-
-  mermaids.forEach((mermaid, index) => {
-    const code = mermaid.querySelector("code");
-
-    if (!code) {
-      return;
-    }
-
-    const mermaidId = `mermaid_${index}_${key}`;
-    const promise = window.mermaid.render(mermaidId, code.textContent || "");
-    promise
-      .then((object) => {
-        mermaid.innerHTML = object.svg;
+    generateDiagram(mermaidSrc)
+      .then((svg) => {
+        mermaidDiagram.innerHTML = svg;
       })
-      .catch((e) => {
-        mermaid.innerText = e?.message || e;
-        // mermaid injects an error element, we need to remove it
-        document.getElementById(mermaidId)?.remove();
-      })
-      .finally(() => {
-        mermaid.dataset.processed = true;
-        mermaid.querySelector(".spinner")?.remove();
+      .catch((error) => {
+        const errorDiv = document.createElement("div");
+        errorDiv.classList.add("alert", "alert-error");
+        errorDiv.textContent = error.message;
+        mermaidDiagram.replaceChildren(errorDiv);
       });
-
-    if (key === "composer") {
-      discourseDebounce(updateMarkdownHeight, mermaid, index, container, 500);
-    }
-  });
-}
-
-function updateMarkdownHeight(mermaid, index, container) {
-  const appEvents = container.lookup("service:app-events");
-  const composer = container.lookup("service:composer");
-
-  let height = parseInt(mermaid.getBoundingClientRect().height, 10);
-  let calculatedHeight = parseInt(mermaid.dataset.calculatedHeight, 10);
-
-  if (height === 0) {
-    return;
   }
 
-  if (height !== calculatedHeight) {
-    mermaid.dataset.calculatedHeight = height;
-
-    const regex = /```mermaid((\s*)|.*auto)$/gm;
-    const existing = [...composer.model.reply.matchAll(regex)][index]?.[0];
-
-    appEvents.trigger(
-      `composer:replace-text`,
-      existing,
-      "```mermaid height=" + height + ",auto",
-      { regex, index }
-    );
-  }
+  mermaidPre.replaceWith(mermaidWrapper);
 }
 
 export default apiInitializer("1.13.0", (api) => {
@@ -114,15 +60,15 @@ export default apiInitializer("1.13.0", (api) => {
 
   if (api.decorateChatMessage) {
     api.decorateChatMessage((element) => {
-      applyMermaid(element, `chat_message_${element.id}`, api.container);
+      element
+        .querySelectorAll("pre[data-code-wrap=mermaid]")
+        .forEach((mermaidPre, helper) => applyMermaid(element, helper));
     });
   }
 
-  api.decorateCookedElement(
-    async (elem, helper) => {
-      const id = helper ? `post_${helper.getModel().id}` : "composer";
-      applyMermaid(elem, id, api.container);
-    },
-    { id: "discourse-mermaid-theme-component" }
-  );
+  api.decorateCookedElement((element, helper) => {
+    element
+      .querySelectorAll("pre[data-code-wrap=mermaid]")
+      .forEach((mermaidPre) => applyMermaid(mermaidPre, helper));
+  });
 });
